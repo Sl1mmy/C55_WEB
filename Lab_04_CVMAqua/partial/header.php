201358e88d2660d75777e43c1ad90d5bef16bb46
Sl1mmy <noebousquet2004@gmail.com>
2023-09-05 12:16:05 -0400

    commit

---
+++ Lab_04_CVMAqua/partial/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>CVMAQUA</title>
+        <link href="css/global.css" rel="stylesheet" media="screen" />
+        <link href="css/jquery.lightbox-0.5.css" rel="stylesheet" media="screen" />
+        <script src="js/jquery-1.4.2.min.js"></script>
+        <script src="js/jquery.lightbox-0.5.min.js"></script>
+		<script src="https://notes-de-cours.com/dev/toggle-lab-info.js"></script>
+    </head>
+    <body>
+		<div class="site-container">
+		    <div class="page-container">
+		    	<div class="site-header">
+		    		<div class="page-title-section">
+			    		<h1>CVMAQUA - Groupe de protection de l'eau</h1>
+			    		<h2>Accueil</h2>
+		    		</div>
+		    		<div class="menu-section">
+		    			<ul>
+		    				<li><a href="index.php">Accueil</a></li>
+		    				<li><a href="images.php">Galerie photos</a></li>
+		    				<li><a href="contact.php">Contactez-nous</a></li>
+		    			</ul>
+		    		</div>
+		    		<div class="clear"></div>
+		    	</div>
+		    	<div class="page-content">
