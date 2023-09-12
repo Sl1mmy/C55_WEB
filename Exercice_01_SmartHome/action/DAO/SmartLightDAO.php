594681c382be8d29c4ccb9354f22536520e0cbaa
Sl1mmy <noebousquet2004@gmail.com>
2023-09-12 11:06:44 -0400

    commit

---
+++ Exercice_01_SmartHome/action/DAO/SmartLightDAO.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("action/DAO/Connection.php");
+
+    class SmartLightDAO {
+        
+
+        public static function getLightsStatus() {
+            $connection = Connection::getConnection();
+
+            $statement = $connection->prepare("SELECT * FROM lights");
+            $statement->setFetchMode(PDO::FETCH_ASSOC); // Juste pour les select, permet de retourner un dictionnaire ex : $row["username"] (sinon $row[0])
+            $statement->execute();
+
+            $lights = $statement->fetchAll();
+
+            
+        
+            return $lights;
+        }
+
+    }
